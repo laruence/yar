7b2242e422d8f7e172fa481a3af7e65ad51c117c
Xinchen Hui <laruence@gmail.com>
2018-08-14 13:38:41 +0800

    Fixed build

--- transports/curl.c
+++ transports/curl.c
@@ -445,7 +445,11 @@ int php_yar_curl_send(yar_transport_interface_t* self, yar_request_t *request, c
 	DEBUG_C(ZEND_ULONG_FMT": pack request by '%.*s', result len '%ld', content: '%.32s'", 
 			request->id, 7, ZSTR_VAL(payload), ZSTR_LEN(payload), ZSTR_VAL(payload) + 8);
 
+#if PHP_VERSION_ID < 70300
 	php_yar_protocol_render(&header, request->id, data->host->user, data->host->pass, ZSTR_LEN(payload), 0);
+#else
+	php_yar_protocol_render(&header, request->id, ZSTR_VAL(data->host->user), ZSTR_VAL(data->host->pass), ZSTR_LEN(payload), 0);
+#endif
 
 	smart_str_appendl(&data->postfield, (char *)&header, sizeof(yar_header_t));
 	smart_str_appendl(&data->postfield, ZSTR_VAL(payload), ZSTR_LEN(payload));
