37a80111ff69aa7ce8fb481fe3ef28dc03b5eaa6
Xinchen Hui <laruence@gmail.com>
2024-06-25 11:21:46 +0800

    release 2.3.3

--- php_yar.h
+++ php_yar.h
@@ -37,7 +37,7 @@ extern zend_module_entry yar_module_entry;
 #include "TSRM.h"
 #endif
 
-#define PHP_YAR_VERSION  "2.3.3-dev"
+#define PHP_YAR_VERSION  "2.3.3"
 
 PHP_MINIT_FUNCTION(yar);
 PHP_MSHUTDOWN_FUNCTION(yar);
